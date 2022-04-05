d145c55abccf8e79d095dc276ee358efe76d5380
gtno1chun <gtno1chun@gmail.com>
2022-04-05 17:53:58 +0900

    test argoCD

---
+++ argo-cd.tf
@@ -0,0 +1,21 @@
+resource "kubernetes_namespace" "argocd-ns" {
+  metadata {
+    annotations = {
+      name = "argocd"
+    }
+    labels = {
+      role = "argocd"
+    }
+    name = argocd
+  }
+}
+
+resource "helm_release" "argocd" {
+  depends_on = [kubernetes_namespace.argocd-ns]
+
+  name = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart = "argo/argo-cd"
+  version = "4.5.0"
+  
+}
