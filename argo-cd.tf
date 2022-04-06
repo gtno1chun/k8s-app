8d211aaf8d639b405152030a149337c4fd0c0ab6
gtno1chun <gtno1chun@gmail.com>
2022-04-06 10:28:55 +0900

    Update argo-cd.tf

--- argo-cd.tf
+++ argo-cd.tf
@@ -14,7 +14,7 @@ resource "helm_release" "argo-cd" {
   depends_on = [kubernetes_namespace.argocd_ns]
 
   name          = "argo-cd"
-  namespace     = kubernetes_namespace.argocd_ns.name
+  namespace     = kubernetes_namespace.argocd_ns.metadata.name
   repository    = "https://argoproj.github.io/argo-helm"
   chart         = "argo-cd"
   version       = "4.5.0"
