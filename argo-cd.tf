03e9602ec3989e77e0a172e39c859eced39935fd
gtno1chun <gtno1chun@gmail.com>
2022-04-06 09:57:11 +0900

    set argo-cd

--- argo-cd.tf
+++ argo-cd.tf
@@ -1,20 +1,20 @@
-resource "kubernetes_namespace" "argocd-ns" {
+resource "kubernetes_namespace" "argocd_ns" {
   metadata {
     annotations = {
-      name = "argocd"
+      name = "argo-cd"
     }
     labels = {
-      role = "argocd"
+      role = "argo-cd"
     }
-    name = "argocd"
+    name = "argo-cd"
   }
 }
 
 resource "helm_release" "argocd" {
-  depends_on = [kubernetes_namespace.argocd-ns]
+  depends_on = [kubernetes_namespace.argocd_ns]
 
   name          = "argocd"
-  namespace     = kubernetes_namespace.argocd-ns
+  namespace     = kubernetes_namespace.argocd_ns
   repository    = "https://argoproj.github.io/argo-helm"
   chart         = "argo-cd"
   version       = "4.5.0"
