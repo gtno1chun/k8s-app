da74e96910b7dc2bf30b4786251f4f0af122bec0
gtno1chun <gtno1chun@gmail.com>
2022-04-11 17:03:34 +0900

    test rm eck

--- eck-operator.tf
+++ eck-operator.tf
@@ -1,22 +1,24 @@
-resource "kubernetes_namespace" "eck_ns" {
-  metadata {
-    annotations = {
-      name = "eck"
-    }
-    labels = {
-      role = "eck"
-    }
-    name = "eck"
-  }
-}
+# resource "kubernetes_namespace" "eck_ns" {
+#   metadata {
+#     annotations = {
+#       name = "eck"
+#     }
+#     labels = {
+#       role = "eck"
+#     }
+#     name = "eck"
+#   }
+# }
 
-resource "helm_release" "eck-operator" {
-  depends_on = [kubernetes_namespace.eck_ns]
+# resource "helm_release" "eck-operator" {
+#   depends_on = [kubernetes_namespace.eck_ns]
 
-  name          = "eck-operator"
-  namespace     = kubernetes_namespace.eck_ns.metadata[0].name
-  repository    = "https://helm.elastic.co"
-  chart         = "eck-operator"
-  version       = "2.1.0"
+#   name          = "eck-operator"
+#   namespace     = kubernetes_namespace.eck_ns.metadata[0].name
+#   repository    = "https://helm.elastic.co"
+#   chart         = "eck-operator"
+#   version       = "2.1.0"
 
-}
+# }
+
+
