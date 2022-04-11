1af85e1e7fabe473cd7d6e9a09748389f960167c
gtno1chun <gtno1chun@gmail.com>
2022-04-11 16:46:57 +0900

    test-eck

---
+++ eck-operator.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_namespace" "eck_ns" {
+  metadata {
+    annotations = {
+      name = "eck"
+    }
+    labels = {
+      role = "eck"
+    }
+    name = "eck"
+  }
+}
+
+resource "helm_release" "eck-operator" {
+  depends_on = [kubernetes_namespace.eck_ns]
+
+  name          = "eck-operator"
+  namespace     = kubernetes_namespace.argocd_ns.metadata[0].name
+  repository    = "https://helm.elastic.co"
+  chart         = "eck-operator"
+  version       = "2.1.0"
+
+}
