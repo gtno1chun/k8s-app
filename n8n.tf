0fe873a4233b83562f0a1a49e1d58851e0ac4540
gtno1chun <gtno1chun@gmail.com>
2022-04-22 12:47:34 +0900

    Update n8n.tf

--- n8n.tf
+++ n8n.tf
@@ -10,13 +10,13 @@ resource "kubernetes_namespace" "n8n_ns" {
   }
 }
 
-# resource "helm_release" "n8s" {
-#   depends_on = [kubernetes_namespace.n8n_ns]
+resource "helm_release" "n8s" {
+  depends_on = [kubernetes_namespace.n8n_ns]
 
-#   name          = "n8n"
-#   namespace     = kubernetes_namespace.n8n_ns.metadata[0].name
-#   repository    = "https://8gears.container-registry.com/chartrepo/library/"
-#   chart         = "n8n"
-#   version       = "0.5.0"
+  name          = "n8n"
+  namespace     = kubernetes_namespace.n8n_ns.metadata[0].name
+  repository    = "https://8gears.container-registry.com/chartrepo/library/"
+  chart         = "n8n"
+  version       = "0.5.0"
 
-# }
+}
