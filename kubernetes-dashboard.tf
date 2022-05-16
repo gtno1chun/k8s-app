d2060e30cb2763d3930dc6a05bf4d2b33ce1f9fe
gtno1chun <gtno1chun@gmail.com>
2022-05-16 10:59:56 +0900

    Update kubernetes-dashboard.tf

--- kubernetes-dashboard.tf
+++ kubernetes-dashboard.tf
@@ -1,9 +1,21 @@
-resource "helm_release" "k8s-dashboard" {
-  name        = "kubernetes-dashboard" 
-  repository  = "./helm/charts"
-  chart       = "kubernetes-dashboard" 
-  version     = "5.4.1"
-
-  values = [ "${file("./helm/charts/kubernetes-dashboard/values.yaml")}" ] 
-
+resource "kubernetes_namespace" "k8s-dashboard" {
+  metadata {
+    annotations = {
+      name = "kubernetes-dashboard"
+    }
+    labels = {
+      role = "kubernetes-dashboard"
+    }
+    name = "kubernetes-dashboard"
+  }
 }
+
+# resource "helm_release" "k8s-dashboard" {
+#   name        = "kubernetes-dashboard" 
+#   repository  = "./helm/charts"
+#   chart       = "kubernetes-dashboard" 
+#   version     = "5.4.1"
+
+#   values = [ "${file("./helm/charts/kubernetes-dashboard/values.yaml")}" ] 
+
+# }
