1e276e56d54927e8269e5785e91d5d6b44ad445e
gtno1chun <gtno1chun@gmail.com>
2022-05-20 14:01:24 +0900

    set vault using other workspace output vaule

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "cluster_workspace" {
 #   default     = "aws secret key"
 # }
 
+### apply vault info -> using my-k8s outputs 
 # variable "vault_endpoint" {
 #   default = ""
 # }
-
 # variable "VAULT_TOKEN" {
 #   description = "vault access Token : https://registry.terraform.io/providers/hashicorp/vault/latest/docs#token" 
 #   sensitive   = true 
