694062ff254e11ada8d840eaf9309b098338f0cf
gtno1chun <gtno1chun@gmail.com>
2022-05-31 15:18:37 +0900

    test security group id

--- test-security-g-id.tf
+++ test-security-g-id.tf
@@ -20,3 +20,18 @@ output "security_group_id" {
   value = data.aws_security_groups.test.id
   
 }
+output "security_group_arn" {
+  description = "sgr-id"
+  value = data.aws_security_groups.test.arn
+  
+}
+output "security_group_ids" {
+  description = "sgr-id"
+  value = data.aws_security_groups.test.ids
+  
+}
+output "security_group_vpc_ids" {
+  description = "sgr-id"
+  value = data.aws_security_groups.test.vpc_ids
+  
+}
