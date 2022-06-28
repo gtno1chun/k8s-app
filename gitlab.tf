89dfd89e204cd17031153451348ae7e52fbd2caf
gtno1chun <gtno1chun@gmail.com>
2022-06-28 17:21:31 +0900

    set up gitlab test

--- gitlab.tf
+++ gitlab.tf
@@ -1,23 +1,23 @@
-resource "kubernetes_namespace" "gitlab_ns" {
-  metadata {
-    annotations = {
-      name = "gitlab"
-    }
-    labels = {
-      role = "gitlab"
-    }
-    name = "gitlab"
-  }
-}
+# resource "kubernetes_namespace" "gitlab_ns" {
+#   metadata {
+#     annotations = {
+#       name = "gitlab"
+#     }
+#     labels = {
+#       role = "gitlab"
+#     }
+#     name = "gitlab"
+#   }
+# }
 
-resource "helm_release" "gitlab" {
-  depends_on = [kubernetes_namespace.gitlab_ns]
+# resource "helm_release" "gitlab" {
+#   depends_on = [kubernetes_namespace.gitlab_ns]
 
-  name          = "gitlab"
-  namespace     = kubernetes_namespace.gitlab_ns.metadata[0].name
-  repository    = "./helm/charts"
-  chart         = "gitlab"
-  version       = "6.1.0"
+#   name          = "gitlab"
+#   namespace     = kubernetes_namespace.gitlab_ns.metadata[0].name
+#   repository    = "./helm/charts"
+#   chart         = "gitlab"
+#   version       = "6.1.0"
 
 
-}
+# }
