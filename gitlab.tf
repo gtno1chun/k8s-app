ebe1ed0fbabdf161d9170b2c95807046f8f41dfc
gtno1chun <gtno1chun@gmail.com>
2022-07-06 20:22:10 +0900

    Update gitlab.tf

--- gitlab.tf
+++ gitlab.tf
@@ -1,27 +1,27 @@
-# resource "kubernetes_namespace" "gitlab_ns" {
-#   metadata {
-#     annotations = {
-#       name = "gitlab"
-#     }
-#     labels = {
-#       role = "gitlab"
-#     }
-#     name = "gitlab"
-#   }
-# }
+resource "kubernetes_namespace" "gitlab_ns" {
+  metadata {
+    annotations = {
+      name = "gitlab"
+    }
+    labels = {
+      role = "gitlab"
+    }
+    name = "gitlab"
+  }
+}
 
-# resource "helm_release" "gitlab" {
-#   depends_on = [kubernetes_namespace.gitlab_ns]
+resource "helm_release" "gitlab" {
+  depends_on = [kubernetes_namespace.gitlab_ns]
 
-#   name          = "gitlab"
-#   namespace     = kubernetes_namespace.gitlab_ns.metadata[0].name
-#   repository    = "./helm/charts"
-#   chart         = "gitlab"
-#   version       = "6.1.0" 
+  name          = "gitlab"
+  namespace     = kubernetes_namespace.gitlab_ns.metadata[0].name
+  repository    = "./helm/charts"
+  chart         = "gitlab"
+  version       = "6.1.0" 
 
-#   values = [ 
-#     file("./helm/charts/gitlab/values.yaml")
-#   ]
+  values = [ 
+    file("./helm/charts/gitlab/values.yaml")
+  ]
 
 
-# }
+}
